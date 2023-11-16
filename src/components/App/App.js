79052b51202f0825fa31eb44afc4bc6f7821bd86
Josh <joshualeebernard@gmail.com>
2023-11-16 10:34:08 +0000

    Add background color and title to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Test</h1>
-      </header>
+    <div>
+      <h1 className="Title" >Jammming</h1>
+      <div className="App">
+
+      </div>
     </div>
   );
 }
