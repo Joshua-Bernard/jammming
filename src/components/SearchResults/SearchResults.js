3ff792e6946891e6caa77a82f9e5cd741b9aacd7
Josh <joshualeebernard@gmail.com>
2023-11-25 06:30:27 +0000

    Update SearchResults component styling and add TrackList component to SearchResults component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './SearchResults.css';
+import React from "react";
+import "./SearchResults.css";
+import TrackList from "../TrackList/TrackList";
 
 function SearchResults(props) {
-
-    return (
-        <div className="SearchResults">
-            <h2>Results</h2>
-            {/* <!-- Add a TrackList component --> */}
-        </div>
-    )
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={props.results} />
+    </div>
+  );
 }
 
 export default SearchResults;
