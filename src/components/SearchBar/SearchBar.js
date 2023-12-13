2abd0b6f8c2dc07b60bdc94a3a05b932ea1340bc
Josh <joshualeebernard@gmail.com>
2023-12-13 10:12:59 +0000

    Refactor SearchBar component to use hooks and add search functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './SearchBar.css';
+import React, { useState, useCallback } from "react";
 
-function SearchBar(props) {
+import "./SearchBar.css";
 
-    return (
-        <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
-        </div>
-    )
-}
+const SearchBar = (props) => {
+  const [term, setTerm] = useState("");
+
+  const handleTermChange = useCallback((event) => {
+    setTerm(event.target.value);
+  }, []);
+
+  const search = useCallback(() => {
+    props.onSearch(term);
+  }, [props.onSearch, term]);
+
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song Title" onChange={handleTermChange} />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
